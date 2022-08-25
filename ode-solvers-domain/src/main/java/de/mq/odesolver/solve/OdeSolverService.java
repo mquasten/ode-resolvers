c4c0588bfe4d464d849eaf325fb647f5463cb8d8
Manfred Quasten <manfred.quasten@t-online.de>
2022-08-25 13:50:42 +0200

    add algorithms from model

--- ode-solvers-domain/src/main/java/de/mq/odesolver/solve/OdeSolverService.java
+++ ode-solvers-domain/src/main/java/de/mq/odesolver/solve/OdeSolverService.java
@@ -1,16 +1,24 @@
 package de.mq.odesolver.solve;
 
-
 public interface OdeSolverService {
-	
-	enum Algorithm {
-		EulerPolygonal,
-		RungeKutta2ndOrder,
-		RungeKutta4ndOrder;
+
+	public enum Algorithm {
+		EulerPolygonal(1), RungeKutta2ndOrder(2), RungeKutta4ndOrder(4);
+
+		private final int order;
+
+		Algorithm(final int order) {
+			this.order = order;
+		}
+
+		public final int order() {
+			return order;
+		}
+
 	}
-	
+
 	OdeSolver odeSolver(final Algorithm algorithm, final String function);
-	
-	double validateRightSide(final String function, final double y0[], final double x0 );
+
+	double validateRightSide(final String function, final double y0[], final double x0);
 
 }
--- ode-solvers-web/src/main/java/de/mq/odesolver/solve/support/SolveController.java
+++ ode-solvers-web/src/main/java/de/mq/odesolver/solve/support/SolveController.java
@@ -1,9 +1,15 @@
 
 package de.mq.odesolver.solve.support;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
@@ -20,6 +26,7 @@ import org.springframework.web.servlet.ModelAndView;
 import de.mq.odesolver.Result;
 import de.mq.odesolver.solve.OdeSolver;
 import de.mq.odesolver.solve.OdeSolverService;
+import de.mq.odesolver.solve.OdeSolverService.Algorithm;
 
 @Controller
 class  SolveController  {
@@ -32,6 +39,7 @@ class  SolveController  {
 
 	
 	private final Map<String, ModelAndView> commands;
+	private final Collection<Entry<String,String>> algorithms=     Arrays.asList(Algorithm.values()).stream().sorted( (a1,a2) -> a2.order() -a1.order() ).map(a ->new SimpleImmutableEntry<String,String>(a.name(),a.name())).collect(Collectors.toList());
 			
 	@Autowired
 	SolveController(final OdeSolverService odeSolverService, final ResultsExcelView resultsExcelView, final ResultsGraphView resultsGraphView, final Converter<OdeModel, Ode> odeConverter) {
@@ -43,7 +51,7 @@ class  SolveController  {
 
 	@GetMapping("/solve")
 	public ModelAndView solve(final Model model) {
-		
+		model.addAttribute("algorithms", algorithms);
 		model.addAttribute("ode", new OdeModel());
 		
 		return solveModelAndView;
@@ -56,7 +64,7 @@ class  SolveController  {
 	public ModelAndView solveSubmit(@RequestParam(name = "command") final String command,
 			@ModelAttribute("ode") @Valid final OdeModel odeModel, final BindingResult bindingResult, final Model model) {
 	
-		
+		model.addAttribute("algorithms", algorithms);
 		if (bindingResult.hasFieldErrors()) {
 			return solveModelAndView;
 		}
