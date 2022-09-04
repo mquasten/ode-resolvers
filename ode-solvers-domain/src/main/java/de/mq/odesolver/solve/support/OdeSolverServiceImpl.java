f86f8967363b0824a190f1f0704541f851a4a0bd
Manfred Quasten <manfred.quasten@t-online.de>
2022-09-04 12:20:28 +0200

    format

--- ode-solvers-domain/src/main/java/de/mq/odesolver/solve/support/OdeSolverServiceImpl.java
+++ ode-solvers-domain/src/main/java/de/mq/odesolver/solve/support/OdeSolverServiceImpl.java
@@ -11,21 +11,22 @@ import de.mq.odesolver.support.OdeFunctionUtil;
 
 class OdeSolverServiceImpl implements OdeSolverService {
 
-	private final Map<Algorithm, Class<? extends OdeResultCalculator>> solvers = Map.of(Algorithm.EulerPolygonal,
-			EulerCalculatorImpl.class, Algorithm.RungeKutta2ndOrder, RungeKutta2CalculatorImpl.class,
-			Algorithm.RungeKutta4thOrder, RungeKutta4CalculatorImpl.class);
+	private final Map<Algorithm, Class<? extends OdeResultCalculator>> solvers = Map.of(Algorithm.EulerPolygonal, EulerCalculatorImpl.class, Algorithm.RungeKutta2ndOrder,
+			RungeKutta2CalculatorImpl.class, Algorithm.RungeKutta4thOrder, RungeKutta4CalculatorImpl.class);
 
 	private final OdeFunctionUtil odeFunctionUtil;
 
 	OdeSolverServiceImpl(final OdeFunctionUtil odeFunctionUtil) {
 		this.odeFunctionUtil = odeFunctionUtil;
-	}
+	} 
+	
+	
+	
 
 	@Override
 	public final OdeSolver odeSolver(final Algorithm algorithm, final String function) {
 		try {
-			final OdeResultCalculator odeResultCalculator = solvers.get(algorithm)
-					.getDeclaredConstructor(OdeFunctionUtil.class, String.class).newInstance(odeFunctionUtil, function);
+			final OdeResultCalculator odeResultCalculator = solvers.get(algorithm).getDeclaredConstructor(OdeFunctionUtil.class, String.class).newInstance(odeFunctionUtil, function);
 			return new OdeSolverImpl(odeResultCalculator);
 		} catch (Exception exception) {
 			throw new IllegalStateException(exception.getCause());
